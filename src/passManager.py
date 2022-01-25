5417a81fa97425b1e0ab870b9e1341326abd13f8
lzzzam <58335995+lzzzam@users.noreply.github.com>
2022-01-26 00:30:13 +0100

    Added basic Drive connections

--- src/passManager.py
+++ src/passManager.py
@@ -1,11 +1,25 @@
 import os
-from passGenerator import randPass
+import os.path
 from tkinter import *
+
+from passGenerator import randPass
+
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from googleapiclient.errors import HttpError
+
+# If modifying these scopes, delete the file token.json.
+SCOPES = ['https://www.googleapis.com/auth/drive.metadata.readonly']
 
 ROOT_PATH = os.path.dirname(__file__)
 IMG_PATH = os.path.realpath(os.path.join(ROOT_PATH, '..', 'img', 'Lock.gif'))
 KEY_DIR_PATH = os.path.realpath(os.path.join(ROOT_PATH, '..', 'key'))
 KEY_CSV_PATH = os.path.join(KEY_DIR_PATH, 'keys.csv')
+
+# Google API credentials
+creds = None
 
 if not os.path.isdir(KEY_DIR_PATH):
     os.mkdir(KEY_DIR_PATH)
@@ -24,9 +38,47 @@ def addNewPass():
     pwd = key_entry.get()
     f.write(f"{website},{email},{pwd}\n")
     
-def syncOnGoogleDrive():
-    pass
+def authorFlow():    
+    global creds
+    
+    if os.path.exists('key/author/token.json'):
+        creds = Credentials.from_authorized_user_file('key/author/token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'key/author/credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('key/author/token.json', 'w') as token:
+            token.write(creds.to_json())
 
+def syncOnGoogleDrive():
+    global creds
+    
+    authorFlow()
+    
+    try:
+        service = build('drive', 'v3', credentials=creds)
+
+        # Call the Drive v3 API
+        results = service.files().list(
+            pageSize=10, fields="nextPageToken, files(id, name)").execute()
+        items = results.get('files', [])
+
+        if not items:
+            print('No files found.')
+            return
+        print('Files:')
+        for item in items:
+            print(u'{0} ({1})'.format(item['name'], item['id']))
+    except HttpError as error:
+        # TODO(developer) - Handle errors from drive API.
+        print(f'An error occurred: {error}')
+        
+        
 def closeProgram():
     f.close()
     window.destroy()
